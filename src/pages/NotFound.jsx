c706dd554685bf2c947acce9d9ec54f338a76750
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2024-01-30 23:50:42 -0500

    NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { FaExclamationCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <FaExclamationCircle className='text-red-500 text-6xl mb-4' />
+      <h1 className='text-5xl mb-3'>404 - Página no encontrada</h1>
+      <p className='text-xl mb-4'>Lo sentimos, la página que buscas no existe.</p>
+      <Link to="/" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        Volver al inicio
+      </Link>
     </div>
-  )
+  );
 }
 
 export default NotFound;
