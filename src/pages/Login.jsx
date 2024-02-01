5f72552fecfeb2321707971051de502420a7f7ce
kevin-alexis <kevinlevin367@outlook.com>
2024-02-01 10:40:15 -0500

    Add function to login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,6 +34,8 @@ function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log('correo: ' + email);
+        console.log('contraseña: ' + password);
         if (!email || !password) {
             setEmailErrorMessage('Por favor, ingresa tu correo electrónico');
             setPasswordErrorMessage('Por favor, ingresa tu contraseña');
@@ -41,13 +43,16 @@ function Login() {
         }
         
         try {
-            const response = await fetch('http://localhost:8080/fitzone/login', {
+            const response = await fetch('https://localhost:8080/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ email, password })
-            });
+            }).then(response => response.json())
+            .then(data => {
+              console.log(data);
+            })
             if (response.ok) {
                 // Autenticación exitosa
                 console.log('Inicio de sesión exitoso');
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -40,7 +40,7 @@ function Register() {
             return;
         }
         try {
-            const response = await fetch('http://localhost:8080/fitzone/users', {
+            const response = await fetch('https://localhost:8080/api/users', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
