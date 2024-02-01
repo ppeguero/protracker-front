e4fc0d34c2001921978d1632fd6e4e061cd440db
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2024-02-01 11:08:22 -0500

    select rol id

--- src/components/AddUserModal.jsx
+++ src/components/AddUserModal.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import Swal from 'sweetalert2';
-
 
 const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
   const [newUser, setNewUser] = useState({
@@ -13,7 +12,6 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
 
   const [isAddingUser, setIsAddingUser] = useState(false);
 
-
   function hasOnlySpaces(str) {
     return str.trim() === '';
   }
@@ -24,20 +22,19 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
   };
 
   const handleAddUser = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     // Validación de espacios en blanco
-  if (hasOnlySpaces(newUser.nombre) || hasOnlySpaces(newUser.contraseña) || hasOnlySpaces(newUser.id_rol_id)) {
-    Swal.fire({
-      title: '¡Error!',
-      text: 'Los campos no pueden consistir solo en espacios en blanco.',
-      icon: 'error',
-    });
-    return;
-  }
+    if (hasOnlySpaces(newUser.nombre) || hasOnlySpaces(newUser.contraseña) || hasOnlySpaces(newUser.id_rol_id)) {
+      Swal.fire({
+        title: '¡Error!',
+        text: 'Los campos no pueden consistir solo en espacios en blanco.',
+        icon: 'error',
+      });
+      return;
+    }
 
-  setIsAddingUser(true); // Activa el estado para desactivar el botón
-
+    setIsAddingUser(true); // Activa el estado para desactivar el botón
 
     fetch('https://localhost:8080/api/users/', {
       method: 'POST',
@@ -58,20 +55,22 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
           title: 'Añadido!',
           text: 'El usuario ha sido añadido.',
           icon: 'success',
-        })
+        });
         setNewUser({
           nombre: '',
           correo: '',
           contraseña: '',
           id_rol_id: '',
-        })}).catch((error) => {
+        });
+      })
+      .catch((error) => {
         console.error('Fetch error:', error);
         Swal.fire('Error', 'Hubo un error al añadir el usuario.', 'error');
-      }).finally(() => {
+      })
+      .finally(() => {
         setIsAddingUser(false); // Desactiva el estado después de que se complete la solicitud
-      });;
+      });
   };
-  
 
   return (
     <Modal
@@ -82,17 +81,14 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
       overlayClassName="fixed inset-0 bg-black bg-opacity-50"
     >
       <h2 className="text-2xl font-bold mb-4">Añadir Usuario</h2>
-      <form onSubmit={(e)=>handleAddUser(e)}>
+      <form onSubmit={handleAddUser}>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Nombre del Usuario:
-          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">Nombre del Usuario:</label>
           <input
             required
             className="w-full px-3 py-2 border rounded-md"
             type="text"
             name="nombre"
-            minLength={6}
             value={newUser.nombre}
             onChange={handleInputChange}
             placeholder="Nombre"
@@ -100,9 +96,7 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Correo del Usuario:
-          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">Correo del Usuario:</label>
           <input
             required
             className="w-full px-3 py-2 border rounded-md"
@@ -115,9 +109,7 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Contraseña:
-          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">Contraseña:</label>
           <input
             required
             className="w-full px-3 py-2 border rounded-md"
@@ -130,24 +122,27 @@ const AddUserModal = ({ isOpen, onRequestClose, handleAddOrUpdate }) => {
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            ID del Rol:
-          </label>
-          <input
+          <label className="block text-gray-700 text-sm font-bold mb-2">Rol:</label>
+          <select
             required
             className="w-full px-3 py-2 border rounded-md"
-            type="text"
             name="id_rol_id"
             value={newUser.id_rol_id}
             onChange={handleInputChange}
-            placeholder="ID del Rol"
-          />
+          >
+            <option value="">Seleccionar Rol</option>
+            <option value="1">Admin</option>
+            <option value="2">Usuario</option>
+          </select>
         </div>
 
         <div className="grid grid-cols-2 gap-2">
           <button
-            className={`bg-green-500 hover:bg-green-700 w-full text-white font-bold py-2 px-4 rounded-md ${isAddingUser ? 'opacity-50 cursor-not-allowed disabled' : ''}`}
-            type="submit">
+            className={`bg-green-500 hover:bg-green-700 w-full text-white font-bold py-2 px-4 rounded-md ${
+              isAddingUser ? 'opacity-50 cursor-not-allowed disabled' : ''
+            }`}
+            type="submit"
+          >
             Añadir Usuario
           </button>
           <button
