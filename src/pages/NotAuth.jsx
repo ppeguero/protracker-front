d7909db38b68fcf2e80f90596c493c88db2cb74a
kevin-alexis <kevinlevin367@outlook.com>
2024-02-02 09:18:42 -0500

    Auth

--- src/pages/NotAuth.jsx
+++ src/pages/NotAuth.jsx
@@ -35,6 +35,7 @@ function NotFound() {
         </div>
       </div>
       :
+      
       <Link to={'/'} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
         Inicia Sesión
       </Link>
